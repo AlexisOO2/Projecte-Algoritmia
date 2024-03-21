430e566668905ca3888bf172ef560de83b8d0894
Aiden <alexismonroy8@gmail.com>
2024-03-21 20:33:00 +0100

    CLIndex Implemented

--- CLIndex.cc
+++ CLIndex.cc
@@ -2,6 +2,10 @@
 
 using namespace std;
 
+void Usage(){
+    cout << "Usage(): ./CLIndex filename ndimensions nclusters" << endl;
+    exit(-1);
+}
 
 double Cal_Har_Index( vector < vector <Point>> &clusters, vector <Point> &centroids){
 	double CH = 0.0;
@@ -44,6 +48,73 @@ double Cal_Har_Index( vector < vector <Point>> &clusters, vector <Point> &centro
 
 	CH = (BCSS)/(WCSS) * ((total_punts - nclusters)/ (nclusters-1));
 	
-
 	return CH;
+}
+
+vector <vector <Point>> readclusters(const string filename, int d, int nclusters){
+ 	vector <vector <Point>> clusters (nclusters);
+
+    string line,word;
+    ifstream file(filename); //Abrimos el fichero de entrada
+    //Leemos cada fila
+    while (getline(file, line)) {
+        stringstream lineStream(line);
+        Point p; // Inicializamos el punto
+
+        // Leemos el valor de cada columna y lo guardamos en los atributos del punto.
+        for (int i = 0; i < d; i++) {
+                getline(lineStream, word, ';');
+                p.vd.push_back(stod(word));
+        }
+        getline(lineStream,word,';');
+
+        int cluster = stoi(word);
+        //inicializamos el clúster al que pertenece en 0.
+        clusters[cluster].push_back(p);
+    }
+    return clusters;
+}
+
+vector <Point> calc_centroids(vector < vector <Point>>& clusters){
+	vector <Point> centroids;
+	vector <Point> sumas (clusters.size());
+    for (int j = 0; j < clusters.size(); j++){
+        sumas[j].vd = vector <double> (clusters[0][0].vd.size(),0.0);
+    }
+	for (int i = 0; i < clusters.size(); ++i){
+		for (int j = 0; j < clusters[i].size(); ++j){
+			for (int k = 0; k < clusters[0][0].vd.size(); ++k){
+	            sumas[i].vd[k] += clusters[i][j].vd[k];
+			}
+		}
+	}
+	for (int i = 0; i < clusters.size(); ++i){
+			for (int k = 0; k < clusters[0][0].vd.size(); ++k){
+	            sumas[i].vd[k] /= clusters[i].size();
+			}
+		centroids.push_back(sumas[i]);
+	}
+	return centroids;
+}
+
+int main(int argc, char *argv[]){
+	string filename = ""; // file = Dataset
+    int d = -1; // d = Nº Dimensiones
+    int k = -1; // k = Nº Clústers
+
+    if (argc != 4) Usage();
+    else {
+            filename = argv[1];
+            d = atoi(argv[2]);
+            k = atoi(argv[3]);
+
+    }
+
+
+	vector < vector <Point>> clusters = readclusters(filename,d,k);	
+	vector <Point> centroids = calc_centroids(clusters);
+
+ 
+	double CH = Cal_Har_Index(clusters,centroids);
+	cout <<"Calinski–Harabasz Index = " << CH << endl;
 }
