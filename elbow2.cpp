900da98d6226abbc0542244016a8035ef92e463d
Aiden <alexismonroy8@gmail.com>
2024-03-22 13:00:33 +0100

    Parcial Implementation

--- elbow2.cpp
+++ elbow2.cpp
@@ -11,7 +11,6 @@ void Usage(){
 void kmeanspp(vector <Point>& points, int iterations, int num_dimensions, vector <pair<int,double> > &Elbow, int &c){
     double sumaact = -1;
     double ultimasum = -1;
-    //cout <<"dins"<<endl;
     while (ultimasum/sumaact > 1.01 or c < 3) {
         vector <Point> centroids (c);
         vector <Point> ant_centroids (c);
@@ -35,7 +34,6 @@ void kmeanspp(vector <Point>& points, int iterations, int num_dimensions, vector
                         distmin = dist;
                         centproper = k;
                     }
-                    //sumaelbow += distmin;
                 }
                 //actualitzem distancia maxima com la distancia més llunyana d'un punt al cluster més proper
                 double dist2 = punto.distance(centroids[centproper]);
@@ -48,17 +46,6 @@ void kmeanspp(vector <Point>& points, int iterations, int num_dimensions, vector
             centroids[i].cluster = i;
         }
         
-        /*
-        for (int i = 0; i < num_clusters; ++i){
-            cout << "Cluster " << centroids[i].cluster << " points: " << endl; 
-            for (int j = 0; j < num_dimensions; ++j){
-                cout << centroids[i].vd[j] << " ";
-            }
-            cout << endl;
-        }
-        */
-        
-
         bool convergence = false;
 
         for (int i = 0; i < iterations and (not convergence) ; i++){
@@ -77,8 +64,6 @@ void kmeanspp(vector <Point>& points, int iterations, int num_dimensions, vector
                 double dist_min = INT_MAX;
                 for (int k = 0; k < c; ++k){
                     double dist = punto.distance(centroids[k]);
-                    //cout << "distancia = " << dist << " al centroide " << k << endl;
-
                     if (dist < dist_min){
                         dist_min = dist;
                         points[j].cluster = k;
@@ -95,12 +80,6 @@ void kmeanspp(vector <Point>& points, int iterations, int num_dimensions, vector
             
 
             for (int l = 0; l < c; ++l){
-                //cout <<"cluster " << l << " tiene: " << endl;
-                //cout << puntos_x_cluster[l] << " puntos, su suma es igual a: ";
-
-                //for (int m = 0; m < num_dimensions; ++m) cout << sumas[l].vd[m]  << " ";
-                //cout << "y su mean es ";
-
                 Point mean;
                 for (int m = 0; m < num_dimensions; ++m){
                     mean.vd.push_back(sumas[l].vd[m]/puntos_x_cluster[l]);
@@ -109,16 +88,8 @@ void kmeanspp(vector <Point>& points, int iterations, int num_dimensions, vector
                 ant_centroids[l] = centroids[l];
 
                 centroids[l] = mean;
-
-                //cout << "(";
-
-                for (int m = 0; m < num_dimensions; ++m){
-                    //cout << mean.vd[m];
-                    //if (m == 0) cout  << ",";
-                }
-                //cout << ")" << " [" << i << "]"<< endl;
+                
             }
-            cout << endl;
 
             for (int j = 0; j < c and (not convergence); ++j){
                 bool change = false; 
@@ -172,7 +143,7 @@ int main(int argc, char *argv[]) {
     int d = -1; // d = Nº Dimensiones
     bool labelled = false; //etiq = Dataset etiquetat
     int k = -1; // k = Nº Clústers
-    //double sumaelbow = 0.0;
+
     vector <pair<int,double> > Elbow(20);
     int c = 1;
 
@@ -185,9 +156,6 @@ int main(int argc, char *argv[]) {
             d = atoi(argv[3]);
     }
     if (argc >= 5) labelled = (atoi(argv[4]) == 1);
-    //if (argc == 6) k = atoi(argv[5]);
-
-    //if (labelled) d++; 
 
     vector<Point> points = readcsv(filename,d);
 
